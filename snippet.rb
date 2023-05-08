92306550e7ca88c693c7032ecb54806f9aa40aa5
VMware Code <noreply@vmware.com>
2023-05-08 03:23:46 +0000

    Initial commit by CHRIS BEYER

---
+++ snippet.rb
@@ -0,0 +1,21 @@
+<?xml version='1.0' encoding='utf-8' ?>
+    <Snippets  xmlns='http://schemas.microsoft.com/PowerShell/Snippets'>
+        <Snippet Version='1.0.0'>
+            <Header>
+                <Title>dtc</Title>
+                <Description>attrib</Description>
+                <Author></Author>
+                <SnippetTypes>
+                    <SnippetType>Expansion</SnippetType>
+                </SnippetTypes>
+            </Header>
+
+            <Code>
+                <Script Language='PowerShell' CaretOffset='0'>
+                    <![CDATA[T&T]]>
+                </Script>
+            </Code>
+
+    </Snippet>
+</Snippets>
+
